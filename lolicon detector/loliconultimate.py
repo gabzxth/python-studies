ecca358e58d249e00937ae795c7c3bc77e9414fa
Gabrielx <47926200+gabzxth@users.noreply.github.com>
2019-04-16 19:33:49 -0300

    16/04/2019 - added lolicon detector

---
+++ lolicon detector/loliconultimate.py
@@ -0,0 +1,46 @@
+print('Olá, qual seu nome?')
+nome = input()
+print('Olá, ' + nome)
+print('Qual sua idade?')
+homem = input()
+if(int(homem) <= 18):
+    print('Qual a idade da sua namorada?')
+    mulher = input()
+    if(int(homem) <= 12) or (int(mulher) <12):
+        print('Toma no cu vão joga roblox carario')
+        input('Press ENTER to exit')
+    elif(int(mulher) <= 18):
+        print('Tá ok, pode passar, ' + nome)
+        input('Press ENTER to exit')
+    elif(int(mulher) > 18):
+        print('TÁ PRESA A FILHA DA PUTA')
+        input('Press ENTER to exit')
+if(int(homem) > 18):
+    print('Qual a idade da sua namorada?')
+    mulher = input()
+    if(int(mulher) <12):
+        print(nome + ', ESTEJA PRESO EM NOME OF THE FUCKING LAW')
+        input('Press ENTER to exit')
+    elif(int(mulher) <= 18):
+            print(nome + ', ESTEJA PRESO EM NOME OF THE FUCKING LAW')
+            input('Press ENTER to exit')
+    elif(int(mulher) > 18):
+            if(int(mulher) <= 40):
+                print('Tá ok, pode passar, ' + nome + ' suggar daddy')
+                input('Press ENTER to exit')
+            if(int(mulher) > 40):
+                list = [homem, mulher]
+                maximo = max(list)
+                minimo = min(list)
+                dif = (int(maximo) - int(minimo))
+                if(int(dif) <= 20):
+                    print('Tá ok, pode passar, ' + nome)
+                    input('Press ENTER to exit')
+                elif(int(dif) > 20):
+                    print('Opa opa opa, temos um/a SUGGAR DADDY/MOMMY')
+                    input('Press ENTER to exit')
+            
+    
+
+    
+    
